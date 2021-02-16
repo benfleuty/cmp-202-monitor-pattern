d917a10aedfea81594af956922bad91590949955
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-02-16 13:20:01 +0000

    default

--- threads/threads.cpp
+++ threads/threads.cpp
@@ -13,22 +13,20 @@ using std::ofstream;
 using std::this_thread::sleep_for;
 using std::thread;
 
-
 void myThreadFunc()
 {
 	sleep_for(seconds(3));
 	cout << "I am myThreadFunc\n";
 }
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	// At the moment our program is only running one thread (the initial one the operating system gave us).
 
 	thread myThread(myThreadFunc);
 
 	// Now our program is running two threads in parallel (the initial one, and myThread).
-	
+
 	cout << "I am main\n";
 
 	// Wait for myThread to finish.
